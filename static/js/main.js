8b7a181f27404571ccb604675d96c4ec65ad2f5c
ran-sy <raneem.1999.ta@gmail.com>
2023-06-18 06:22:29 +0300

    multipe table/ indicators disabable

--- static/js/main.js
+++ static/js/main.js
@@ -39,64 +39,84 @@ const events = () => {
     $('.navbar').each(function(){
         $('<nav class="full-width flex flex-start"><div class="dropdown"><div class="icon public bgDark material-icons">notes</div><div class="dropdownOptions"><li><i class="material-icons icon public">checklist</i> TODO</li></div></div></nav>').appendTo($(this))
     })
+
     $('table.paginated').each(function() {
         var currentPage = 0;
         var numPerPage = $(this).data('size') || 10;
         var $table = $(this);
         $table.bind('repaginate', function() {
             $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-            $('.table-wrapper').find('.page-number').addClass('hide')
-            $('.table-wrapper').find(`.page-number:nth-child(${currentPage+1})`).removeClass('hide');
+            $(this).parents('.table-wrapper').find('.page-number').addClass('hide')
+            $(this).parents('.table-wrapper').find(`.page-number:nth-child(${currentPage+1})`).removeClass('hide');
         });
         $table.trigger('repaginate');
         var numRows = $table.find('tbody tr').length;
         var numPages = Math.ceil(numRows / numPerPage);
+        var numTo;
         var $pager = $('<div class="pager"></div>');
-        for (var page = 0; page < numPages; page++) {
-            $(`<p class="page-number">${page*numPerPage+1}-${(1+page)*numPerPage} of ${numRows}</p>`).appendTo($pager).addClass('hide');
+
+        function btnDisabling(btn, isToggled){
+            $(btn).siblings().attr("disabled", false); //enable sibling btn
+            if(isToggled){
+                $(btn).attr("disabled", true);
+            }
+            else $(btn).attr("disabled", false);
+            $table.trigger('repaginate'); 
         }
-        $('<span id="back" class="icon public bgDark radius0 rotate180 material-icons" disabled>arrow_forward_ios</span>').bind('click', function(){
-            console.log(currentPage)
-            if(currentPage != 0) currentPage--;
-            $table.trigger('repaginate');
+
+        for (var page = 0; page < numPages; page++) {
+            numTo = (1+page)*numPerPage <= numRows?(1+page)*numPerPage : numRows; 
+            $(`<p class="page-number">${page*numPerPage+1}-${numTo} of ${numRows}</p>`).appendTo($pager).addClass('hide');
+        }
+
+        $(`<span id="back" class="icon public bgDark radius0 rotate180 material-icons" ${currentPage == 0? 'disabled':''}>arrow_forward_ios</span>`).bind('click', function(){
+            if(currentPage != 0) {
+                currentPage--;
+                btnDisabling($(this), currentPage == 0)
+            }
         }).appendTo($pager);
-        $('<span id="forward" class="icon public bgDark radius0 material-icons">arrow_forward_ios</span>').bind('click', function(){
-            console.log(currentPage)
-            if(numPages != currentPage+1) currentPage++;
-            $table.trigger('repaginate');
+        $(`<span id="forward" class="icon public bgDark radius0 material-icons" ${numPages == currentPage+1? 'disabled':''}>arrow_forward_ios</span>`).bind('click', function(){
+            if(numPages != currentPage+1){
+                currentPage++;
+                btnDisabling($(this), numPages == currentPage+1)
+            }
         }).appendTo($pager);
             
         
-        $('.table-wrapper').prepend($pager).find('.page-number:first').removeClass('hide');
+        $(this).parents('.table-wrapper').prepend($pager).find('.page-number:first').removeClass('hide');
 
-        $('td.checkbox').each(function(){
-            const title = $(this).data('title') || 'title';
-            const desc = $(this).data('description') || 'description';
-            $(`<div class="checkmark"></div><div><h5 class="cHover">${title}</h5> <p class="desc">${desc}</p></div>`).appendTo($(this))
-        })
-        $('td.btns').each(function(){
-            const number = parseInt($(this).data('number')) || 1;
-            const colors = Array.from(Object.values($(this).data('colors'))) || '';
-            const names = Array.from(Object.values($(this).data('names'))) || '';
-            let button =[];
-            for(let i=0; i<number; i++)
-            button[i]=`<div class="button small ${colors[i]}"> ${names[i]}</div>`
-            $(`<div class="flex">${button.join(' ')}</div>`).appendTo($(this))
-        })
-        $('td.date').each(function(){
-            date = $(this).data('date')? $(this).data('date'):'Aug, 07 2020';
-            $(`<p class='c0 one-line'>${date}</p>`).appendTo($(this))
-        })
-        $('td.profileImg').each(function(){
-            imgUrl =$(this).data('url')? `<img class='avatar small' src=${$(this).data('url')} />` : 'JS';
-            $(`<div class="avatar small">${imgUrl}</div>`).appendTo($(this))
-        })
-        $('td.menu').each(function(){
-            $('<div class="dropdown"><i class="icon public material-icons">unfold_more_double</i><div class="dropdownOptions small"><a href="" class="li"><i class="icon public material-icons">edit</i>Edit</a><a href="" class="li"><i class="icon public material-icons">delete</i>delete</a><a href="" class="li"><i class="icon public material-icons">star</i>Important</a></div></div>').appendTo($(this))
-        })
     });
+    $('td.checkbox').each(function(){
+        const title = $(this).data('title') || 'title';
+        const desc = $(this).data('description') || 'description';
+        $(`<div class="checkmark"></div><div><h5 class="cHover">${title}</h5> <p class="desc">${desc}</p></div>`).appendTo($(this))
+    })
+    $('td.btns').each(function(){
+        const number = parseInt($(this).data('number')) || 1;
+        const colors = Array.from(Object.values($(this).data('colors'))) || '';
+        const names = Array.from(Object.values($(this).data('names'))) || '';
+        let button =[];
+        for(let i=0; i<number; i++)
+        button[i]=`<div class="button small ${colors[i]}"> ${names[i]}</div>`
+        $(`<div class="flex">${button.join(' ')}</div>`).appendTo($(this))
+    })
+    $('td.date').each(function(){
+        date = $(this).data('date')? $(this).data('date'):'Aug, 07 2020';
+        $(`<p class='c0 one-line'>${date}</p>`).appendTo($(this))
+    })
+    $('td.profileImg').each(function(){
+        imgUrl =$(this).data('url')? `<img class='avatar small' src=${$(this).data('url')} />` : 'JS';
+        $(`<div class="avatar small">${imgUrl}</div>`).appendTo($(this))
+    })
+    $('td.menu').each(function(){
+        $('<div class="dropdown"><i class="icon public material-icons">unfold_more_double</i><div class="dropdownOptions small"><a href="" class="li"><i class="icon public material-icons">edit</i>Edit</a><a href="" class="li"><i class="icon public material-icons">delete</i>delete</a><a href="" class="li"><i class="icon public material-icons">star</i>Important</a></div></div>').appendTo($(this))
+    })
 }
 
 $(document).ready(()=>{
     events();
 })
+
+// in pagination make the max num = row num
+// check why cant add 2 tables correctly
+//  on each .pagination it recreate the first element
